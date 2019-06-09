fc045badd96402c910fc326cba09b28ab2d52115
Alexander Markard <alexandermarkard@gmail.com>
2019-06-09 03:48:41 +0200

    first simple header file

---
+++ init.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define HP 1.5707963267948966192
+#define OP 3.1415926535897932385
+#define DP 6.2831853071795864769
+#define SR 96000.0
+#define HSR 48000.0
+#define HN 0.94387431268169349664
