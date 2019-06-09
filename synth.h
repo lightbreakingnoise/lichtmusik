00ed4335d190f087bf8e8b787a4b1593dbb28215
Alexander Markard <alexandermarkard@gmail.com>
2019-06-09 05:19:44 +0200

    makes more sense now

--- synth.h
+++ synth.h
@@ -1,25 +1,20 @@
 typedef struct {
 	double phase;
 	double inc;
+	double vol;
 	double lfo_phase;
 	double lfo_inc;
 	double lo;
 	int c;
 } ISYNTH, *PSYNTH;
 
-PSYNTH synth_create(int tune, double lfo_hz) {
+PSYNTH synth_create(double lfo_hz) {
 	PYSNTH syn;
 	syn = (PSYNTH) malloc(sizeof(ISYNTH));
 
-	double f;
-	f = 27.5;
-
-	int i;
-	for (i = -3; i < tune; i++)
-		f /= HN;
-
 	syn->phase = 0.0;
-	syn->inc = (2.0 / SR) * f;
+	syn->inc = 0.0;
+	syn->vol = 1.0;
 	syn->lfo_phase = 0.0;
 	syn->lfo_inc = (DP / SR) * lfo_hz;
 	syn->lo = 0.0;
@@ -32,7 +27,6 @@ double synth_walk(PSYNTH syn) {
 	double out;
 
 	out = syn->phase - 1.0;
-
 	syn->phase += syn->inc + (0.01 * sin(syn->lfo_phase) * syn->inc);
 	if (syn->phase > 1.0)
 		syn->phase -= 2.0;
@@ -48,7 +42,7 @@ double synth_walk(PSYNTH syn) {
 		q = 0.1;
 
 	syn->lo -= (syn->lo - out) * q;
-	out = syn->lo;
+	out = syn->lo * syn->vol;
 
 	syn->c = (syn->c + 1) % 12;
 
