29c1563b38e7887181761c86c0d2b68db9c2e691
Alexander Markard <alexandermarkard@gmail.com>
2019-06-09 05:21:27 +0200

    simple example for building a song

---
+++ example.c
@@ -0,0 +1,44 @@
+#include "init.h"
+#include "song.h"
+#include "synth.h"
+#include "tune.h"
+
+int main() {
+	tune_create();
+
+	PSONG song = song_create(125.0);
+	PSYNTH syn = synth_create(1.7);
+
+	double sam;
+	short pcm;
+	int c = 0;
+
+	while (c < 1024) {
+		if (song_walk(song) == 1) {
+			c = song->c;
+			if (c % 8 == 0) {
+				syn->inc = saw_TABLE[24];
+				syn->vol = 1.0;
+			}
+			if (c % 8 == 3) {
+				syn->inc = saw_TABLE[21];
+				syn->vol = 1.0;
+			}
+			if (c % 8 == 6) {
+				syn->inc = saw_TABLE[19];
+				syn->vol = 1.0;
+			}
+		}
+
+		sam = synth_walk(syn, incsyn);
+
+		if (sam < -1.0) sam = -1.0;
+		if (sam > 1.0) sam = 1.0;
+		out = (short)(sam * 32767.0);
+		fwrite(&out, 2, 1, stdout);
+		fwrite(&out, 2, 1, stdout);
+		fflush(stdout);
+	}
+
+	return 0;
+}
