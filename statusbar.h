ea4a5aaf1a5a195163b27155deb314081a9baec8
Alexander Markard <alexandermarkard@gmail.com>
2019-06-09 06:53:26 +0200

    adding a simple progress bar module

---
+++ statusbar.h
@@ -0,0 +1,33 @@
+typedef struct {
+	int progress;
+	int prog_old;
+	char txt[50];
+} ISTATUS, *PSTATUS;
+
+PSTATUS sbar_create() {
+	PSTATUS sbar;
+	sbar = (PSTATUS) malloc(sizeof(ISTATUS));
+
+	sbar->progress = 0;
+	sbar->prog_old = 0;
+	memset(sbar->txt, 0, 50);
+	sbar->txt[0] = '\r';
+
+	return sbar;
+}
+
+void sbar_show(PSTATUS sbar, int amount, int end) {
+	int i;
+	int x;
+
+	sbar->progress = (amount * 48) / end;
+	if (sbar->prog_old != sbar->progress) {
+		x = sbar->progress;
+		sbar->prog_old = x;
+		for (i = 1; i < 48; i++) {
+			if (x > i) txt[i] += '=';
+			else txt[i] += ' ';
+		}
+		fwrite(txt, 1, 48, stderr);
+	}
+}
